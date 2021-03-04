0b7f45a3ed7a24b34504a8fc56be7344fa0f9bbf
JuliaVal821 <julia.kolgatina@gmail.com>
2021-03-03 21:58:06 -0500

    Implemented a function that applies the standard fizz buzz test

---
+++ Fizz Buzz - Without ifs.js
@@ -0,0 +1,6 @@
+https://www.codewars.com/kata/584c702256c98fc0a0000043/train/javascript
+
+    const fizzBuzz = n => {
+        return n % 15 === 0 ? "FizzBuzz"
+            : n % 3 === 0 ? "Fizz" : n % 5 === 0 ? "Buzz" : n;
+    };
